ac5cc4788675bf7823f05154c0c53d0c68980c09
Mauro Caritas <mfcaritasbdos@gmail.com>
2026-04-22 18:51:48 -0500

    Avance con el primero login de flowbite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,39 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <h2>Iniciar Sesión</h2>
-    <form id="loginForm">
-        <input type="email" id="email" placeholder="Correo electrónico"><br><br>
-        <input type="password" id="password" placeholder="Contraseña"><br><br>
-        <label><input type="checkbox" id="remember">Recordar</label><br><br>
-        <button type="submit">Iniciar Sesión</button>
+<body>    
+    <form class="max-w-sm mx-auto">
+        <div class="mb-5">
+            <label for="email" class="block mb-2.5 text-sm font-medium text-gray-900">Correo electrónico</label>
+            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-3 py-2.5 shadow-sm placeholder-gray-400" required />
+        </div>
+        <div class="mb-5">
+            <label for="password" class="block mb-2.5 text-sm font-medium text-heading">Your password</label>
+            <input type="password" id="password" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="••••••••" required />
+        </div>
+        <label for="remember" class="flex items-center mb-5">
+            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft" required />
+            <p class="ms-2 text-sm font-medium text-heading select-none">I agree with the <a href="#" class="text-fg-brand hover:underline">terms and conditions</a>.</p>
+        </label>
+        <button type="submit" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Submit</button>
     </form>
-    <script>
-        document.getElementById('loginForm').addEventListener('submit',async function (e) {
-            e.preventDefault();
-            let response = await fetch('/api/login',{
-                method:'POST',
-                headers:{
-                    'Content-Type':'application/json'
-                },
-                body:JSON.stringify({
-                    email:document.getElementById('email').value,
-                    password:document.getElementById('password').value,
-                })
-            });
-
-            let data = await response.json();
-
-            if (data.access_token) {
-                localStorage.setItem('token',data.access_token);
-                localStorage.setItem('role',data.role);
-                if(data.role === 'admin') window.location.href = '/admin/dashboard';
-            } else {
-                alert(data.message);
-            }
-        });
-    </script>
 </body>
 </html>
